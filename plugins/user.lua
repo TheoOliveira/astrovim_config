db151b0c9939f831ad849c85e46b86cbe8aeb76c
TheoOliveira <theophilo.oliveira@gmail.com>
2023-06-15 09:08:44 -0300

    commit
    changes: added ale and auto pair

--- plugins/user.lua
+++ plugins/user.lua
@@ -2,6 +2,9 @@ return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
+  "windwp/nvim-autopairs",
+  "dense-analysis/ale"
+
   -- {
   --   "ray-x/lsp_signature.nvim",
   --   event = "BufRead",
